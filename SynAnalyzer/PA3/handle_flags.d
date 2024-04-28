6463cc3da05240a7069148d1a9f81d94e29e5ff1
Fernando Luiz <44549332+feeluiz@users.noreply.github.com>
2024-04-28 12:27:38 -0300

    Initial Schema

--- SynAnalyzer/PA3/handle_flags.d
+++
@@ -1,4 +0,0 @@
-handle_flags.o handle_flags.d : handle_flags.cc /var/tmp/cool/include/PA3/copyright.h \
- /var/tmp/cool/include/PA3/cool-io.h \
- /var/tmp/cool/include/PA3/copyright.h \
- /var/tmp/cool/include/PA3/cgen_gc.h
--- SynAnalyzer/PA3/tree.d
+++
@@ -1,4 +0,0 @@
-tree.o tree.d : tree.cc /var/tmp/cool/include/PA3/copyright.h \
- /var/tmp/cool/include/PA3/tree.h /var/tmp/cool/include/PA3/copyright.h \
- /var/tmp/cool/include/PA3/stringtab.h /var/tmp/cool/include/PA3/list.h \
- /var/tmp/cool/include/PA3/cool-io.h
